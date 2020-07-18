12208ef48992ccdfdbee7da004ad7f43877e374b
hsuhau <hsuhau@foxmail.com>
2020-07-18 17:21:51 +0800

    SpringBoot原理分析

---
+++ single-node-system/spring-boot-study-hello/src/main/java/com/hsuhau/neteaseseniorjava/singlenodesystem/controller/MainController.java
@@ -0,0 +1,18 @@
+package com.hsuhau.neteaseseniorjava.singlenodesystem.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author hsuhau
+ * @date 2020/7/18 16:12
+ */
+@Controller
+public class MainController {
+    @GetMapping("/hello")
+    @ResponseBody
+    public String toMain() {
+        return "Hello Spring Boot!";
+    }
+}
