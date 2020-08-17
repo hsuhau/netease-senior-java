5bb9dcd8f599ad5b1df933f8d5bd3e10b3271cc8
hsuhau <hsuhau@foxmail.com>
2020-08-17 15:37:36 +0800

    3.3.2 BIO网络编程

---
+++ single-node-system/tomcat/network-programming/src/main/java/com/hsuhau/bio/BIOServerMultithreading.java
@@ -0,0 +1,55 @@
+package com.hsuhau.bio;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * 多线程
+ *
+ * @author hsuhau
+ * @date 2020/7/23 17:29
+ */
+public class BIOServerMultithreading {
+    private static ExecutorService threadPool = Executors.newCachedThreadPool();
+
+    public static void main(String[] args) throws Exception {
+        ServerSocket serverSocket = new ServerSocket(8080);
+        System.out.println("服务启动成功！");
+        while (!serverSocket.isClosed()) {
+            // 阻塞
+            Socket request = serverSocket.accept();
+            System.out.println("收到新请求：" + request.toString());
+
+            threadPool.execute(() -> {
+                try {
+                    // 接收数据、打印
+                    InputStream inputStream = request.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+                    String msg;
+                    // 阻塞
+                    while ((msg = reader.readLine()) != null) {
+                        if (msg.length() == 0) {
+                            break;
+                        }
+                        System.out.println(msg);
+                    }
+                    System.out.println("收到数据，来自：" + request.toString());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        request.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+    }
+}
